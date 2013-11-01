fe596e9fcf259f63e27c02a5483e1542fca0cb42
StephenSmithwick <Stephen.Smithwick@gmail.com>
2013-11-01 16:06:16 +1100

    using d3js and angular to render svg map

---
+++ app/assets/javascripts/charts-resources.js
@@ -0,0 +1,71 @@
+angular
+.module('lhd', ['ngResource', 'ng-rails-csrf'])
+.factory('LhdMetrics', function($resource) {
+  return $resource('lhd_metrics/:id', {id: '@id'}, {
+    update: { method: 'PUT' },
+    create: { method: 'POST' }
+  });
+})
+.factory('Lhds', function($resource) {
+  return $resource('lhds/:id', {id: '@id'}, {
+    update: { method: 'PUT' },
+    create: { method: 'POST' }
+  });
+})
+.factory('Metrics', function($resource) {
+  return $resource('metrics/:id', {id: '@id'}, {
+    update: { method: 'PUT' },
+    create: { method: 'POST' }
+  });
+})
+.directive('barChart', function(){
+  var chart = d3.custom.barChart();
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="chart"></div>',
+    scope:{
+      data: '=data',
+      selected: '&selected'
+    },
+    link: function(scope, element, attrs) {
+      var chartEl = d3.select(element[0]);
+      chart.on('selected', function(d, i){
+        scope.selected({args:d});
+      });
+
+      scope.$watch('data', function (data) {
+        chartEl.datum(data).call(chart);
+      });
+    }
+  }
+})
+.directive('mapChart', function(){
+  var chart = d3.custom.mapChart();
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'assets/directives/charts.html',
+    scope:{
+      data: '=data',
+      selected: '&selected'
+    },
+    link: function(scope, element, attrs) {
+      var chartEl = d3.select(element[0]);
+      chart.on('selected', function(d, i){
+        scope.selected({args:d});
+      });
+
+      scope.$watch('data', function (data) {
+        chartEl.datum(data).call(chart);
+      });
+    }
+  }
+});
+
+// default actions
+// 'get':    {method:'GET'}
+// 'save':   {method:'POST'}
+// 'query':  {method:'GET', isArray:true}
+// 'remove': {method:'DELETE'}
+// 'delete': {method:'DELETE'} 
